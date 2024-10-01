8960b4f64d59fa72079246255ab8bd78593fee90
tonhoIcomp <antonio.fernandes@icomp.ufam.edu.br>
2024-09-30 20:26:47 -0400

    criar pdf

--- BotEleitor/pdf/pdf.py
+++ BotEleitor/pdf/pdf.py
@@ -1,17 +1,14 @@
 from fpdf import FPDF
 import os
 
-# Função para gerar o arquivo PDF
 def gerar_pdf(caminho_diretorio, nro_cpf, titulo, situacao, secao, zona, local_votacao, endereco, bairro, municipio_uf, pais):
-    # Criar uma instância do FPDF
+ 
     pdf = FPDF()
     pdf.add_page()
     
-    # Definir o título do documento
     pdf.set_font("Arial", 'B', 16)
     pdf.cell(200, 10, txt="Informações do Título Eleitoral", ln=True, align='C')
 
-    # Adicionar os detalhes no PDF
     pdf.set_font("Arial", size=12)
     
     pdf.cell(200, 10, txt=f"Número do título: {titulo}", ln=True)
@@ -24,23 +21,17 @@ def gerar_pdf(caminho_diretorio, nro_cpf, titulo, situacao, secao, zona, local_v
     pdf.cell(200, 10, txt=f"Município/UF: {municipio_uf}", ln=True)
     pdf.cell(200, 10, txt=f"País: {pais}", ln=True)
     
-    # Definir o nome do arquivo no formato nroCPF_titulo.pdf
     nome_arquivo = f"{nro_cpf}_titulo.pdf"
-    
-    # Definir o caminho completo do arquivo
+ 
     caminho_completo = os.path.join(caminho_diretorio, nome_arquivo)
 
-    # Verificar se o diretório existe, caso contrário, criar
     if not os.path.exists(caminho_diretorio):
         os.makedirs(caminho_diretorio)
-    
-    # Salvar o arquivo no diretório especificado
+ 
     pdf.output(caminho_completo)
     
     print(f"PDF gerado com sucesso em: {caminho_completo}")
 
-
-# Exemplo de uso da função
 if __name__ == "__main__":
  
     caminho_diretorio = "C:\provafinal\API-BotEleitor\BotEleitor\pdf"
@@ -55,6 +46,5 @@ if __name__ == "__main__":
     bairro = "Centro"
     municipio_uf = "São Paulo/SP"
     pais = "Brasil"
-    
-    # Gerar o PDF
+ 
     gerar_pdf(caminho_diretorio, nro_cpf, titulo, situacao, secao, zona, local_votacao, endereco, bairro, municipio_uf, pais)
