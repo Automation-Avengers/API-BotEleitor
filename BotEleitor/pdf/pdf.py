2fca3b334ea590266ee2b2bdfd4c64714eee0967
tonhoIcomp <antonio.fernandes@icomp.ufam.edu.br>
2024-09-30 20:19:24 -0400

    criar pdf feito

--- BotEleitor/pdf/pdf.py
+++ BotEleitor/pdf/pdf.py
@@ -1,69 +1,60 @@
-from PyPDF2 import PdfWriter
-from PyPDF2 import PdfReader
-from PyPDF2 import PdfMerger
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image, Paragraph
-from reportlab.lib import colors
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from datetime import datetime
+from fpdf import FPDF
+import os
+
+# Função para gerar o arquivo PDF
+def gerar_pdf(caminho_diretorio, nro_cpf, titulo, situacao, secao, zona, local_votacao, endereco, bairro, municipio_uf, pais):
+    # Criar uma instância do FPDF
+    pdf = FPDF()
+    pdf.add_page()
+    
+    # Definir o título do documento
+    pdf.set_font("Arial", 'B', 16)
+    pdf.cell(200, 10, txt="Informações do Título Eleitoral", ln=True, align='C')
+
+    # Adicionar os detalhes no PDF
+    pdf.set_font("Arial", size=12)
+    
+    pdf.cell(200, 10, txt=f"Número do título: {titulo}", ln=True)
+    pdf.cell(200, 10, txt=f"Situação: {situacao}", ln=True)
+    pdf.cell(200, 10, txt=f"Seção: {secao}", ln=True)
+    pdf.cell(200, 10, txt=f"Zona: {zona}", ln=True)
+    pdf.cell(200, 10, txt=f"Local de votação: {local_votacao}", ln=True)
+    pdf.cell(200, 10, txt=f"Endereço: {endereco}", ln=True)
+    pdf.cell(200, 10, txt=f"Bairro: {bairro}", ln=True)
+    pdf.cell(200, 10, txt=f"Município/UF: {municipio_uf}", ln=True)
+    pdf.cell(200, 10, txt=f"País: {pais}", ln=True)
+    
+    # Definir o nome do arquivo no formato nroCPF_titulo.pdf
+    nome_arquivo = f"{nro_cpf}_titulo.pdf"
+    
+    # Definir o caminho completo do arquivo
+    caminho_completo = os.path.join(caminho_diretorio, nome_arquivo)
+
+    # Verificar se o diretório existe, caso contrário, criar
+    if not os.path.exists(caminho_diretorio):
+        os.makedirs(caminho_diretorio)
+    
+    # Salvar o arquivo no diretório especificado
+    pdf.output(caminho_completo)
+    
+    print(f"PDF gerado com sucesso em: {caminho_completo}")
 
 
-def merge_pdfs(pdf_lista, arq_saida):
-    merger = PdfMerger()
+# Exemplo de uso da função
+if __name__ == "__main__":
+ 
+    caminho_diretorio = "C:\provafinal\API-BotEleitor\BotEleitor\pdf"
+
+    nro_cpf = "57014540297"
+    titulo = "123456789"
+    situacao = "REGULAR"
+    secao = "123"
+    zona = "456"
+    local_votacao = "Escola Municipal"
+    endereco = "Rua ABC, 123"
+    bairro = "Centro"
+    municipio_uf = "São Paulo/SP"
+    pais = "Brasil"
     
-    for pdf in pdf_lista:
-        merger.append(pdf)
-    
-    merger.write(arq_saida)
-    merger.close()
-  
-def criar_pdf_lista_produto(produtos):
-    arq_logo = 'C:\\Users\\matutino\\Documents\\Atividade3\\botproduto\pdf\\banner.png'
-    arq_destino = 'C:\\Users\\matutino\\Documents\\Atividade3\\botproduto\\ListaProduto.pdf'
-    
-    # Cria o documento PDF
-    pdf = SimpleDocTemplate(arq_destino, pagesize=A4)
-    
-    # Estilos
-    estilos = getSampleStyleSheet()
-    estilo_titulo = estilos['Title']
-    estilo_normal = estilos['Normal']
-    estilo_centralizado = ParagraphStyle(name='Centralizado', parent=estilos['Normal'], alignment=1)  # 1 é para centralizar
-    
-    # Parágrafo de texto
-    # paragrafo = Paragraph(texto, estilo_normal)
-    
-    # Cabeçalho com imagem e título
-    imagem = Image(arq_logo, width=320, height=50)
-    titulo = Paragraph("Lista de Produto", estilo_titulo)
-    
-    # Data e hora atual no formato brasileiro
-    data_hora_atual = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    #paragrafo_data_hora = Paragraph(f"Data e Hora: {data_hora_atual}", estilo_normal)
-    paragrafo_data_hora = Paragraph(f"Data e Hora: {data_hora_atual}", estilo_centralizado)
-    
-    # Cria a tabela de dados
-    dados = [["Descrição", "Unidade", "Quantidade", "Preço Real","Preço Dolar"]]
-    for produto in produtos:
-        dados.append([produto["descricao"], produto["unidade"], produto["quantidade"], produto["preco_real"],produto["preco_dolar"]])
-    
-    # Cria a tabela
-    tabela = Table(dados)
-    
-    # Define o estilo da tabela
-    estilo_tabela = TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-        ('GRID', (0, 0), (-1, -1), 1, colors.black),
-    ])
-    tabela.setStyle(estilo_tabela)
-    
-    # Adiciona os elementos ao documento
-    elementos = [imagem, titulo, paragrafo_data_hora, tabela]
-    pdf.build(elementos)
+    # Gerar o PDF
+    gerar_pdf(caminho_diretorio, nro_cpf, titulo, situacao, secao, zona, local_votacao, endereco, bairro, municipio_uf, pais)
